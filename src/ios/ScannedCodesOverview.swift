3e95b7d69cedbd2a676b463b3c4890831550096e
Mikita Shchyhelski <hokej07@gmail.com>
2020-07-13 15:58:34 +0300

    rm unused code

--- src/ios/ScannedCodesOverview.swift
+++ src/ios/ScannedCodesOverview.swift
@@ -78,22 +78,5 @@ class ScannedCodesOverview: UIViewController, UITableViewDelegate, UITableViewDa
              tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
-    
-
-    @objc func buttonAction() {
-        print("DONE")
-     }
-
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
